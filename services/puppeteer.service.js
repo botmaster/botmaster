773efb1ef12e7cbe2bfa08cdf3fd463348ee341b
Pascal Achard <pascal.achard@gmail.com>
2023-03-29 15:16:41 +0200

    Update puppeteer.service.js

--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -32,7 +32,6 @@ class PuppeteerService {
             await this.init();
         }
         this.page = await this.browser.newPage();
-        await this.page.setCacheEnabled(false);
 
         await this.page.setExtraHTTPHeaders({
             'Accept-Language': 'en-US',
