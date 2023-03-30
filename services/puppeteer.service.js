7f48b871470a3b58ead004b0e6cda146d88fa431
botmaster <pascal.achard@gmail.com>
2023-03-30 15:18:47 +0200

    🎨 Cleanup

--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -1,5 +1,3 @@
-//const puppeteer = require('puppeteer');
-
 import puppeteer from "puppeteer";
 
 class PuppeteerService {
@@ -16,11 +14,9 @@ class PuppeteerService {
                 '--window-position=0,0',
                 '--ignore-certifcate-errors',
                 '--ignore-certifcate-errors-spki-list',
-                '--incognito',
-                // '--proxy-server=http=194.67.37.90:3128',
-                // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
+                '--incognito'
             ],
-             headless: 'new',
+            headless: 'new',
         });
     }
 
@@ -40,7 +36,7 @@ class PuppeteerService {
         });
 
         await this.page.goto(url, {
-            waitUntil: `networkidle0`,
+            waitUntil: 'networkidle0',
         });
     }
 
@@ -58,18 +54,17 @@ class PuppeteerService {
         try {
             const url = `https://www.picuki.com/profile/${acc}`;
             await this.goToPage(url);
-            //await this.page.waitForTimeout(6000);
             console.log('wait for selector', this.page.url());
-            //await this.page.waitForSelector('body');
+
             await this.page.waitForSelector('.post-image');
 
             const images = await this.page.evaluate(() => {
-                const imgs = document.querySelectorAll('.post-image');
+                const imgs = document.querySelectorAll(".post-image");
                 return Array.from(imgs).map(img => img.src);
             });
 
-            console.log("images", images);
-            if(!images || images.length === 0) {
+            console.log('images', images);
+            if (!images || images.length === 0) {
                 console.error('No image found');
                 return [];
             }
